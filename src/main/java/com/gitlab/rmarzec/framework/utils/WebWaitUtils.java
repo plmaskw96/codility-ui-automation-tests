f505e068f8d62265b79dc82a5293feb503a7345d
plmaskw2 <maciek115606@gmail.comn>
2022-08-16 02:47:03 +0200

    Compilation fix

--- src/main/java/com/gitlab/rmarzec/framework/utils/WebWaitUtils.java
+++ src/main/java/com/gitlab/rmarzec/framework/utils/WebWaitUtils.java
@@ -19,28 +19,28 @@ public class WebWaitUtils {
     }
 
     public WebElement waitForVisible(WebElement webElement, long timeout) {
-        return new WebDriverWait(driver, timeout)
+        return new WebDriverWait(driver, Duration.ofSeconds(timeout))
                 .pollingEvery(Duration.ofMillis(200))
                 .until(ExpectedConditions.visibilityOf(webElement));
     }
 
     public List<WebElement> waitForPresenceOfAllElementsBy(By by, long timeout) {
-        return new WebDriverWait(driver, timeout)
+        return new WebDriverWait(driver, Duration.ofSeconds(timeout))
                 .until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
     }
 
     public Boolean waitForPageReadyState(long timeout) {
-    return new WebDriverWait(driver, timeout).until(
+    return new WebDriverWait(driver, Duration.ofSeconds(timeout)).until(
             webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
     }
 
     public WebElement waitForClickable(WebElement webElement, long timeout) {
-        return new WebDriverWait(driver, timeout)
+        return new WebDriverWait(driver, Duration.ofSeconds(timeout))
                 .pollingEvery(Duration.ofMillis(200))
                 .until(ExpectedConditions.elementToBeClickable(webElement));
     }
 
     public void waitForNotVisible(WebElement webElement, long timeout) {
-        new WebDriverWait(driver, timeout).until(ExpectedConditions.invisibilityOf(webElement));
+        new WebDriverWait(driver, Duration.ofSeconds(timeout)).until(ExpectedConditions.invisibilityOf(webElement));
     }
 }
